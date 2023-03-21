2958563190cec966f63887382a3d83b97b5c355b
guDutra <gudutra05@outlook.com>
2023-03-21 01:27:39 -0300

    projeto finalizado

--- JS-ManipulaçãoObjetos/main.js
+++ JS-ManipulaçãoObjetos/main.js
@@ -1,24 +1,119 @@
 let listaDeItens = []
-
+let itemAEditar
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
+const ulItens = document.getElementById("lista-de-itens")
+const ulItensComprados = document.getElementById("itens-comprados")
+const listaRecuperada = localStorage.getItem('listaDeItens')
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault()
+function atualizaLocalStorage() {
+    localStorage.setItem('listaDeItens', JSON.stringify(listaDeItens))
+}
+
+// (valores omitidos, 0, null, NaN, undefined, "", false) << retornam false
+if(listaRecuperada) {
+    listaDeItens = JSON.parse(listaRecuperada);
+    mostrarItem();
+} else {
+    listaDeItens = [];
+}
+
+form.addEventListener("submit", function (evento) {
+    evento.preventDefault()
     salvarItem()
+    mostrarItem()
+    itensInput.focus()
 })
 
 function salvarItem() {
-    const comprasItem = itensInput.value;
+    const comprasItem = itensInput.value
     const checarDuplicado = listaDeItens.some((elemento) => elemento.valor.toUpperCase() === comprasItem.toUpperCase())
 
     if (checarDuplicado) {
-        alert("Item já existe.")
+        alert("Item já existe")
     } else {
         listaDeItens.push({
             valor: comprasItem,
+            checar: false
         })
     }
 
+    itensInput.value = ''
+}
+
+function mostrarItem() {
+    ulItens.innerHTML = ''
+    ulItensComprados.innerHTML = ''
+
+    listaDeItens.forEach((elemento, index) => {
+        if (elemento.checar) {
+            ulItensComprados.innerHTML += `
+    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" checked class="is-clickable" />  
+            <span class="itens-comprados is-size-5">${elemento.valor}</span>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>
+        `
+        } else {
+            ulItens.innerHTML += `
+    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemAEditar) ? 'disabled' : ''}></input>
+        </div>
+        <div>
+        ${index === Number(itemAEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
+        <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>
+    `
+        }
+    })
+
+    const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
+
+    inputsCheck.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            listaDeItens[valorDoElemento].checar = evento.target.checked
+            mostrarItem()
+        })
+    })
+    const deletarObjetos = document.querySelectorAll(".deletar")
+
+    deletarObjetos.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            listaDeItens.splice(valorDoElemento, 1)
+            //listaDeItens.splice(valorDoElemento,0,{valor: "Limão", checar: false}) // adiciona => pelo segundo parametro ser 0 ele adiciona cada vez que clicado
+            // listaDeItens.splice(valorDoElemento,1,{valor: "Limão", checar: false}) // substituição => criando novo objeto para substituir objeto
+            mostrarItem()
+        })
+    })
+
+    const editarItens = document.querySelectorAll(".editar")
+
+    editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value')
+            mostrarItem()
+        })
+    })
+
+    atualizaLocalStorage();
+}
+
+
+
+function salvarEdicao() {
+    const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
+    console.log(itemEditado.value)
+    listaDeItens[itemAEditar].valor = itemEditado.value
     console.log(listaDeItens)
+    itemAEditar = -1;
+    mostrarItem();
 }
