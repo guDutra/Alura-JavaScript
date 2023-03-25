7335ad15dbb5f8f2d8d3495a07a1e09342fc188f
guDutra <gudutra05@outlook.com>
2023-03-25 19:08:52 -0300

    feito até aula4

--- bytebank-javascript-main/README.md
+++ bytebank-javascript-main/README.md
@@ -1,10 +1,3 @@
-<p align="center"> <img src="https://imgur.com/1mc1qX7.png" alt="JavaScript: trabalhando com threads para requisições simultâneas"> </p>
-
-<hr>
-
-<p align="center"> <img src="https://github.com/MonicaHillman/aluraplay-requisicoes/blob/main/img/logo.png" alt="Logo da Alura"> </p>
-<p align="center">Bytebank: o seu banco digital!</p>
-
 ## Assuntos abordados durante o curso
 * Fetch API
 * Threads
---
+++ bytebank-javascript-main/script/scripts.js
@@ -0,0 +1,44 @@
+import imprimeCotacao from "./imprimeCotacao.js";
+
+const graficoDolar = document.getElementById('graficoDolar');
+
+const graficoParaDolar = new Chart(graficoDolar, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Dólar',
+            data: [],
+            borderWidth: 1
+        }]
+    },
+});
+
+// ...
+setInterval(() => conectaAPI(), 5000)
+
+async function conectaAPI() {
+    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
+    const conectaTraduzido = await conecta.json();
+    let tempo = geraHorario();
+    let valor = conectaTraduzido.USDBRL.ask;
+    adicionarDados(graficoParaDolar, tempo, valor);
+    imprimeCotacao("Dólar", valor);
+}
+
+function geraHorario() {
+    let data = new Date();
+    let horario = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+    console.log(horario);
+    return horario;
+}
+
+function adicionarDados(grafico, legenda, dados) {
+    grafico.data.labels.push(legenda);
+    grafico.data.datasets.forEach((dataset) => {
+        dataset.data.push(dados);
+    })
+    grafico.update();
+}
+
+let workerDolar = new Worker('./script/workers/workerDolar.js');
