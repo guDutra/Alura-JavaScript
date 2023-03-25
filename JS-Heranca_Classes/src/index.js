0fff0110474ec67a1bd09c9fdb495dcbe3026cfa
guDutra <gudutra05@outlook.com>
2023-03-25 16:04:05 -0300

    metade da aula5 feita

--- JS-Heranca_Classes/src/index.js
+++ JS-Heranca_Classes/src/index.js
@@ -9,6 +9,7 @@ const magaJulia = new Mago('Julia', 8, 'gelo', 7, 10)
 const arqueiroBruno = new Arqueiro('Bruno', 7, 8)
 const arqueiroDutra = new Arqueiro('Dutra', 4, 4)
 const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
+
 const personagens = [magoAntonio, magaJulia, arqueiroBruno, arqueiroDutra, arqueiroMagoChico]
 console.log(Personagem.verificarVencedor(arqueiroBruno, magoAntonio))
 
--- JS-Heranca_Classes/src/modules/personagem.js
+++ JS-Heranca_Classes/src/modules/personagem.js
@@ -2,17 +2,17 @@ export class Personagem {
     nome
     vida = 100
     mana = 100
-    level
+    #level
     tipo
     descricao
     constructor(nome, level) {
         this.nome = nome
-        this.level = level
+        this.#level = level
 
     }
 
     obterInsignia() {
-        if (this.level >= 5) {
+        if (this.#level >= 5) {
             return `Implacavel ${this.constructor.tipo}`
         }
         return `${this.constructor.tipo} iniciante`
