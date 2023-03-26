ca883a83b5f82993ee9b1d6fdadc80348e276323
guDutra <gudutra05@outlook.com>
2023-03-25 23:09:56 -0300

    projeto finalizado

--- bytebank-javascript-main/script/imprimeCotacao.js
+++ bytebank-javascript-main/script/imprimeCotacao.js
@@ -1,13 +1,24 @@
-const lista = document.querySelector('[data-lista]');
+const lista = document.querySelectorAll('[data-lista]');
 
-function imprimeCotacao(nome, valor) {
+function selecionaCotacao(nome, valor) {
+    lista.forEach((listaEscolhida) => {
+        if (listaEscolhida.id == nome) {
+            imprimeCotacao(listaEscolhida, nome, valor);
+        }
+    })
+}
+
+function imprimeCotacao(lista, nome, valor) {
     lista.innerHTML = '';
-
+    const plurais = {
+        "dolar": "dolares",
+        "iene": "ienes"
+    }
     for (let multiplicador = 1; multiplicador <= 1000; multiplicador *= 10) {
         const listaItem = document.createElement('li');
-        listaItem.innerHTML = `${multiplicador} ${nome}: R$${(valor * multiplicador).toFixed(2)}`
+        listaItem.innerHTML = `${multiplicador} ${multiplicador == 1 ? nome : plurais[nome]}: R$${(valor * multiplicador).toFixed(2)}`
         lista.appendChild(listaItem)
     }
 }
 
-export default imprimeCotacao;
+export default selecionaCotacao;
--- bytebank-javascript-main/script/scripts.js
+++ bytebank-javascript-main/script/scripts.js
@@ -1,4 +1,4 @@
-import imprimeCotacao from "./imprimeCotacao.js";
+import selecionaCotacao from "./imprimeCotacao.js";
 
 const graficoDolar = document.getElementById('graficoDolar');
 
@@ -14,17 +14,9 @@ const graficoParaDolar = new Chart(graficoDolar, {
     },
 });
 
-// ...
-setInterval(() => conectaAPI(), 5000)
 
-async function conectaAPI() {
-    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-    const conectaTraduzido = await conecta.json();
-    let tempo = geraHorario();
-    let valor = conectaTraduzido.USDBRL.ask;
-    adicionarDados(graficoParaDolar, tempo, valor);
-    imprimeCotacao("Dólar", valor);
-}
+
+
 
 function geraHorario() {
     let data = new Date();
@@ -42,3 +34,35 @@ function adicionarDados(grafico, legenda, dados) {
 }
 
 let workerDolar = new Worker('./script/workers/workerDolar.js');
+workerDolar.postMessage("usd");
+
+workerDolar.addEventListener("message", event => {
+    let tempo = geraHorario();
+    let valor = event.data.ask;
+    selecionaCotacao("dolar", valor);
+    adicionarDados(graficoParaDolar, tempo, valor);
+})
+
+const graficoIene = document.getElementById('graficoIene');
+const graficoParaIene = new Chart(graficoIene, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Iene',
+            data: [],
+            borderWidth: 1
+        }]
+    }
+})
+
+
+let workerIene = new Worker("./script/workers/workerIene.js");
+workerIene.postMessage("iene");
+
+workerIene.addEventListener("message", event => {
+    let tempo = geraHorario();
+    let valor = event.data.ask;
+    adicionarDados(graficoParaIene, tempo, valor);
+    selecionaCotacao("iene", valor)
+})
