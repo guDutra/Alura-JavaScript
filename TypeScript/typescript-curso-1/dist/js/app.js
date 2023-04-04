a9265724f4e3164ac0bf21c26ba0ad73f4e142e0
guDutra <gudutra05@outlook.com>
2023-04-04 01:41:18 -0300

    feito até aula 4

--- TypeScript/typescript-curso-1/dist/js/app.js
+++ TypeScript/typescript-curso-1/dist/js/app.js
@@ -1,2 +1,9 @@
 import { Negociacao } from './models/negociacao.js';
+import { NegociacaoController } from './controllers/negociacao-controller.js';
 const negociacao = new Negociacao(new Date(), 10, 100);
+const negociacaoController = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    negociacaoController.adiciona();
+});
