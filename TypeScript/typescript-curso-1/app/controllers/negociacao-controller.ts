41e7cd1e58cfe25dd57c8effa9352ee5b96c9509
guDutra <gudutra05@outlook.com>
2023-04-04 14:43:02 -0300

    curso finalizado

--- TypeScript/typescript-curso-1/app/controllers/negociacao-controller.ts
+++ TypeScript/typescript-curso-1/app/controllers/negociacao-controller.ts
@@ -1,10 +1,12 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
 
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
 
     constructor() {
         this.inputData = document.querySelector("#data");
@@ -14,7 +16,9 @@ export class NegociacaoController {
 
     adiciona(): void {
         const negociacao = this.criaNegociacao();
-        console.log(negociacao);
+        this.negociacoes.adiciona(negociacao);
+        
+        console.log(this.negociacoes.lista())
         this.limparFormulario();
     }
 
